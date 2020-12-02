2d45417a6d3d83a7accc0490d2c189bdd1b8d094
pef <pefbartimee.com@outlook.com>
2020-12-01 21:25:26 -0500

    Add Build Script

---
+++ build.py
@@ -0,0 +1,51 @@
+import os
+import PyInstaller.__main__
+import shutil
+from distutils.dir_util import copy_tree
+
+version = "137"
+
+
+print("\nBuilding Dashboard...")
+PyInstaller.__main__.run([
+    'manager.py',
+    '-i=assets/icon.ico',
+    '-w',
+    '-y',
+    '-n=GWSL'
+])
+
+
+print("\nBuilding Service...")
+PyInstaller.__main__.run([
+    'main.py',
+    '-i=assets/icon.ico',
+    '-w',
+    '-y',
+    '-n=GWSL_service',
+    '--hidden-import=pkg_resources',
+    '--hidden-import=infi.systray'
+])
+
+
+print(f"\nCreating dist/GWSL_{version}")
+try:
+    os.mkdir(f"dist/GWSL_{version}")
+except:
+    print("Deleting Old Build")
+    shutil.rmtree(f"dist/GWSL_{version}")
+    os.mkdir(f"dist/GWSL_{version}")
+
+print("Copying Assets...")
+folders = ["assets", "locale", "PULSE", "PUTTY", "VCXSRV"]
+for folder in folders:
+    print("Merging:", folder)
+    shutil.copytree(folder, f"dist/GWSL_{version}/" + str(folder))
+
+print("Merging: Dashboard...")
+copy_tree("dist/GWSL/", f"dist/GWSL_{version}/")
+
+print("Merging: Service...")
+copy_tree("dist/GWSL_service/", f"dist/GWSL_{version}/")
+
+
